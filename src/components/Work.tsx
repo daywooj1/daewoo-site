fdfb5d466ef8c76ea813d90b2517398940406801
daywooj1 <jeongdaewoo@gmail.com>
2026-03-26 10:50:01 -0400

    update

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -141,7 +141,7 @@ export default function Work() {
           </div>
 
           {/* Grid View Container */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-16">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
             <AnimatePresence mode="popLayout">
               {filteredProjects.map((project) => (
                 <motion.div
